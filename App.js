adf61a9e1b8addcc18af3e246620c24999bfe14e
Jess purvis <jess.purvis@and.digital>
2023-02-15 11:59:55 +0000

    added linting

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import React from "react";
+import { View, Text, Image, ScrollView } from "react-native";
 
 export default function App() {
-	const MovieCard = () => (
-		<View>
-			<Image source={{ uri: "https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" }} style={{ width: 200, height: 350, marginRight: 8 }} />
+  const MovieCard = () => (
+    <View>
+      <Image
+        source={{
+          uri: "https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+        }}
+        style={{ width: 200, height: 350, marginRight: 8 }}
+      />
       <Text>Sing 2</Text>
     </View>
-	)
+  );
 
   return (
     <View>
       <Text>MOST POPULAR MOVIES</Text>
-	    <ScrollView horizontal={true}>
-				<MovieCard />
-				<MovieCard />
-				<MovieCard />
-				<MovieCard />
-	    </ScrollView>
+      <ScrollView horizontal>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </ScrollView>
     </View>
-	);
+  );
 }
