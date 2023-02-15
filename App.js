41be7385177b3e527e004805e169683ae194074f
Jess purvis <jess.purvis@and.digital>
2023-02-15 14:32:31 +0000

    linter added

--- App.js
+++ App.js
@@ -1,38 +1,42 @@
-import React, { useState, useEffect} from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
-import { getPopularMovies } from './src/api/movies';
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, ScrollView } from "react-native";
+
+import { getPopularMovies } from "./src/api/movies";
 
 export default function App() {
   const [popularMovies, setPopularMovies] = useState([]);
 
-	useEffect(() => {
-		fetchPopularMovies();
-	}, []);
-	
-	const fetchPopularMovies = async () => {
-		const fetchedPopularMovies = await getPopularMovies();
-		setPopularMovies(fetchedPopularMovies);
-	}
+  useEffect(() => {
+    fetchPopularMovies();
+  }, []);
 
-	const MovieCard = ({ image, title }) => (
-		<View>
-			<Image source={{ uri: image }} style={{ width: 200, height: 350, marginRight: 8 }} />
+  const fetchPopularMovies = async () => {
+    const fetchedPopularMovies = await getPopularMovies();
+    setPopularMovies(fetchedPopularMovies);
+  };
+
+  const MovieCard = ({ image, title }) => (
+    <View>
+      <Image
+        source={{ uri: image }}
+        style={{ width: 200, height: 350, marginRight: 8 }}
+      />
       <Text>{title}</Text>
     </View>
-	)
+  );
 
   return (
     <View>
       <Text>MOST POPULAR MOVIES</Text>
-	    <ScrollView horizontal={true}>
-      {popularMovies.map(popularMovie => (
+      <ScrollView horizontal>
+        {popularMovies.map((popularMovie) => (
           <MovieCard
             key={popularMovie.title}
             image={popularMovie.posterImage}
             title={popularMovie.title}
           />
         ))}
-	    </ScrollView>
+      </ScrollView>
     </View>
-	);
+  );
 }
