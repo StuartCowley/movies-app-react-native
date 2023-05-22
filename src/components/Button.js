f087ff449a5b7f0d68a430b0080dbaa09114a34a
StuartCowley <voidstalker@live.co.uk>
2023-05-22 12:52:22 +0100

    Replaced Pressable in button with TouchableOpacity

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
-import { Pressable, Text } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 
 import styles from "../styles/Button.styles.js";
 
 const Button = ({ label, onPress }) => {
   return (
-    <Pressable style={styles.buttonWrapper} onPress={onPress}>
+    <TouchableOpacity style={styles.buttonWrapper} onPress={onPress}>
       <Text style={styles.button}>{label}</Text>
-    </Pressable>
+    </TouchableOpacity>
   );
 };
 
